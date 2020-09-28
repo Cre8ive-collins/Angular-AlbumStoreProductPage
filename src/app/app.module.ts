81e81510683f4bdef5576f8c543e0b88b815c9f0
Cre8ive-collins <wilsoncollins44@yahoo.com>
2020-09-28 15:08:53 +0100

    fixed fails

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,9 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
-const appRoutes : Routes[] = [
-  { path : 'products' , component : 'ProductListComponent' },
-  { path: 'product/:id', component: "ProductPageComponent" },
+const appRoutes : Routes = [
+  { path : 'products' , component : ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
   { path: '', redirectTo: 'products', pathMatch:'full' }
 ]
 @NgModule({
@@ -22,13 +22,13 @@ const appRoutes : Routes[] = [
     ProductPageComponent,
     ProductDescriptionComponent,
     ProductTracklistingComponent,
-    ProductListComponent,
-    RouterModule.forRoot(appRoutes)
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
